a1c4b675ce6983bef147662c6077483017c45d90
Himanshu Kumar <himanshu151281@gmail.com>
2024-05-06 09:29:29 +0530

    React Part 5 - Added Feature

--- React_Mini_F/src/InfoBox.jsx
+++ React_Mini_F/src/InfoBox.jsx
@@ -3,10 +3,20 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import "./InfoBox.css";
+import ThunderstormIcon from "@mui/icons-material/Thunderstorm";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
 
 export default function InfoBox({ info }) {
   const INIT_URL =
     "https://images.unsplash.com/photo-1672226405717-697c84f48f9e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGR1c3R5JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+
+  const HOT_URL =
+    "https://images.unsplash.com/photo-1473496169904-658ba7c44d8a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aG90JTIwc2Vhc29ufGVufDB8fDB8fHww";
+  const COLD_URL =
+    "https://plus.unsplash.com/premium_photo-1664376075509-faa4b6a5c5b7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29sZCUyMHNlYXNvbnxlbnwwfHwwfHx8MA%3D%3D";
+  const RAIN_URL =
+    "https://images.unsplash.com/photo-1590502768362-abf8b480bf0c?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
   return (
     <div className="InfoBox">
@@ -16,11 +26,24 @@ export default function InfoBox({ info }) {
             component="img"
             alt="green iguana"
             height="140"
-            image={INIT_URL}
+            image={
+              info.humidity > 80
+                ? RAIN_URL
+                : info.temp > 15
+                ? HOT_URL
+                : COLD_URL
+            }
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
               {info.city}
+              {info.humidity > 80 ? (
+                <ThunderstormIcon />
+              ) : info.temp > 15 ? (
+                <WbSunnyIcon />
+              ) : (
+                <AcUnitIcon />
+              )}
             </Typography>
             <Typography
               variant="body2"
